e9ccdcee02a1df718e6cc7874006708677ce06b3
convict-git <official.mr.convict@gmail.com>
2021-08-19 11:21:19 +0530

    Done

---
+++ advanced-react-hooks/src/exercise/03.js
@@ -0,0 +1,32 @@
+import * as React from 'react'
+
+const CountContext = React.createContext()
+
+function CountProvider(props) {
+  const [count, setCount] = React.useState(0)
+  return <CountContext.Provider value={[count, setCount]} {...props} />
+}
+
+function CountDisplay() {
+  const [count] = React.useContext(CountContext)
+  return <div>{`The current count is ${count}`}</div>
+}
+
+function Counter() {
+  const [, setCount] = React.useContext(CountContext)
+  const increment = () => setCount(c => c + 1)
+  return <button onClick={increment}>Increment count</button>
+}
+
+function App() {
+  return (
+    <div>
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
+    </div>
+  )
+}
+
+export default App
