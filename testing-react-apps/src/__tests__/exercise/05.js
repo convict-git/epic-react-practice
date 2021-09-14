490b9d51bdc558c414a44e77fc0dbb73c0810225
convict-git <official.mr.convict@gmail.com>
2021-09-14 19:07:26 +0530

    05 v6

--- testing-react-apps/src/__tests__/exercise/05.js
+++ testing-react-apps/src/__tests__/exercise/05.js
@@ -5,6 +5,7 @@ import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
+import {rest} from 'msw'
 import {setupServer} from 'msw/node'
 import Login from '../../components/login-submission'
 import {handlers} from 'test/server-handlers'
@@ -16,10 +17,15 @@ const buildLoginForm = build({
   },
 })
 
+const testErrorMsgs = {
+  UNKNOWN_SERVER: 'something is wrong',
+}
+
 const server = setupServer(...handlers)
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
+afterEach(() => server.resetHandlers())
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -56,5 +62,31 @@ test(`omitting the passsword results in an error`, async () => {
   // expect(screen.getByRole('alert')).toHaveTextContent(/password required/i)
   expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
     `"password required"`,
+  )
+})
+
+test(`unknown server error displays the error message`, async () => {
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(
+          ctx.status(500),
+          ctx.json({
+            message: testErrorMsgs.UNKNOWN_SERVER,
+          }),
+        )
+      },
+    ),
+  )
+
+  render(<Login />)
+
+  userEvent.click(screen.getByRole('button'), {name: /submit/i})
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(
+    testErrorMsgs.UNKNOWN_SERVER,
   )
 })
