88adf588c0b4ef5aaa518f9fb42a45696420a2b0
convict-git <official.mr.convict@gmail.com>
2021-09-14 16:25:41 +0530

    02 v2

--- testing-react-apps/src/__tests__/exercise/02.js
+++ testing-react-apps/src/__tests__/exercise/02.js
@@ -3,6 +3,7 @@
 
 import * as React from 'react'
 import {render, fireEvent} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
@@ -12,13 +13,13 @@ test('counter increments and decrements when the buttons are clicked', () => {
     container.querySelectorAll('button')
   const messageDiv = container.firstChild.querySelector('div')
 
-  expect(messageDiv.textContent).toBe('Current count: 0')
+  expect(messageDiv).toHaveTextContent('Current count: 0')
 
   fireEvent.click(incrementButton)
-  expect(messageDiv.textContent).toBe('Current count: 1')
+  expect(messageDiv).toHaveTextContent('Current count: 1')
 
   fireEvent.click(decrementButton)
-  expect(messageDiv.textContent).toBe('Current count: 0')
+  expect(messageDiv).toHaveTextContent('Current count: 0')
 
   // will unmount everything at the end
 })
