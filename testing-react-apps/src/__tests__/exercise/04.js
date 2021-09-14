10ca1dd5b50caa145d3919c6ee89e0edd6144194
convict-git <official.mr.convict@gmail.com>
2021-09-14 17:36:58 +0530

    04 v4

--- testing-react-apps/src/__tests__/exercise/04.js
+++ testing-react-apps/src/__tests__/exercise/04.js
@@ -5,22 +5,29 @@ import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect'
+import faker from 'faker'
 import Login from '../../components/login'
+import {build} from '@jackfranklin/test-data-bot'
+
+const buildLoginForm = overrideFormData => ({
+  username: faker.internet.userName(),
+  password: faker.internet.password(),
+  ...overrideFormData,
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
   render(<Login onSubmit={handleSubmit} />)
-  screen.debug()
+  // screen.debug()
   const userNameTextBox = screen.getByLabelText(/username/i)
   const passwordTextBox = screen.getByLabelText(/password/i)
 
-  const data = {
-    username: 'chucknorris',
-    password: 'i need no password',
-  }
+  const data = buildLoginForm()
 
   userEvent.type(userNameTextBox, data.username)
   userEvent.type(passwordTextBox, data.password)
+
+  console.log(data)
 
   userEvent.click(screen.getByRole('button', {name: /submit/i}))
 
