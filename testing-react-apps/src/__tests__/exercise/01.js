b4d76fe7a60b004d573ba86ffd896febb13b55fb
convict-git <official.mr.convict@gmail.com>
2021-09-14 15:55:27 +0530

    01 v2

--- testing-react-apps/src/__tests__/exercise/01.js
+++ testing-react-apps/src/__tests__/exercise/01.js
@@ -1,10 +1,16 @@
 // simple test with ReactDOM
 // http://localhost:3000/counter
 
+import {click} from '@testing-library/user-event/dist/click'
 import {context} from 'msw'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import Counter from '../../components/counter'
+
+beforeEach(() => {
+  // accepts a callback to be run before each test
+  document.body.innerHTML = '' // initialised with empty string (so stupid, unmount?)
+})
 
 test('counter increments and decrements when the buttons are clicked', () => {
   const div = document.createElement('div')
@@ -14,10 +20,27 @@ test('counter increments and decrements when the buttons are clicked', () => {
   const messageDiv = div.firstChild.querySelector('div')
 
   expect(messageDiv.textContent).toBe('Current count: 0')
-  incrementButton.click()
+
+  const clickEvent = new MouseEvent('click', {
+    bubbles: true, // allowing event delegation
+    cancelable: true,
+    button: 0, // primary click (generally, left click)
+  })
+
+  incrementButton.dispatchEvent(clickEvent)
+
   expect(messageDiv.textContent).toBe('Current count: 1')
-  decrementButton.click()
+
+  const decrementClickEvent = new MouseEvent('click', {
+    bubbles: true, // allowing event delegation
+    cancelable: true,
+    button: 0, // primary click (generally, left click)
+  })
+
+  decrementButton.dispatchEvent(clickEvent)
+
   expect(messageDiv.textContent).toBe('Current count: 0')
+
   div.remove()
 })
 
