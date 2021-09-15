a42eff0b4fd4c48b125de14399bca919f5008df3
convict-git <official.mr.convict@gmail.com>
2021-09-15 11:18:59 +0530

    07 v3

--- testing-react-apps/src/__tests__/exercise/07.js
+++ testing-react-apps/src/__tests__/exercise/07.js
@@ -18,4 +18,16 @@ test('renders with the light styles for the light theme', () => {
   `) // ThemeProvider
 })
 
+test('renders with the dark styles for the dark theme', () => {
+  const Wrapper = ({children}) => {
+    return <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
+  }
+  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: black;
+    color: white;
+  `) // ThemeProvider
+})
+
 /* eslint no-unused-vars:0 */
