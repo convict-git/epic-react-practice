4f0ecc9faa48e39657dcc59f5b9a27230a493c84
convict-git <official.mr.convict@gmail.com>
2021-09-15 11:15:36 +0530

    07 v1

--- testing-react-apps/src/__tests__/exercise/07.js
+++ testing-react-apps/src/__tests__/exercise/07.js
@@ -7,16 +7,16 @@ import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
 test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
-  // render(<EasyButton>Easy</EasyButton>)
-  // const button = screen.getByRole('button', {name: /easy/i})
-  // expect(button).toHaveStyle(`
-  //   background-color: white;
-  //   color: black;
-  // `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
+  render(
+    <ThemeProvider>
+      <EasyButton>Easy</EasyButton>
+    </ThemeProvider>,
+  )
+  const button = screen.getByRole('button', {name: /easy/i})
+  expect(button).toHaveStyle(`
+    background-color: white;
+    color: black;
+  `) // ThemeProvider
 })
 
 /* eslint no-unused-vars:0 */
